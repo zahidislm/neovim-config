cf01d5ea0d4b5b2bf1c2aa169a88fd0fdb365443
Zahid Islam <dev@zahidislm.com>
2022-11-19 16:10:00 -0500

    Removed Hop & several other unused plugins

--- lua/user/plugins/config/others.lua
+++ lua/user/plugins/config/others.lua
@@ -11,19 +11,6 @@ local M = {}
 M.autopairs = function()
     vim.g.AutoPairsShortcutToggle = ""
     map("i", "<C-l>", "<Esc><cmd>call AutoPairsJump()<CR>a", s_opts)
-    local group = augroup("custom_autopairs", { clear = true })
-    autocmd("Filetype", {
-        command = [[let b:AutoPairs = AutoPairsDefine({'<' : '>'})]],
-        desc = "Add angle brackets as an autopair.",
-        group = group,
-        pattern = { "lua", "vim", "md", "html", "xml" },
-    })
-    autocmd("Filetype", {
-        command = [[let b:AutoPairs = {'(':')', '[':']', '{':'}', "'":"'", '"':'"', "`":"'", '``':"''"}]],
-        desc = "Add latex quotes as autopairs.",
-        group = group,
-        pattern = "tex",
-    })
 end
 
 M.nvim_comment = function()
@@ -50,49 +37,6 @@ M.lsp_signature = function()
         handler_opts = {
             border = "single",
         },
-    })
-end
-
-M.hop = function()
-    require("hop").setup({ jump_on_sole_occurrence = true })
-    map("n", "S", "<cmd>HopChar2<CR>", s_opts)
-    map("n", "f", "<cmd>HopChar1<CR>", s_opts)
-    map("o", "f", function()
-        require("hop").hint_char1({
-            direction = require("hop.hint").HintDirection.AFTER_CURSOR,
-            current_line_only = true,
-        })
-    end)
-end
-
-M.markdown_preview = function()
-    -- https://github.com/wbthomason/packer.nvim/issues/620
-    vim.cmd("doautocmd mkdp_init BufEnter")
-    vim.g.mkdp_auto_close = 0
-    vim.g.mkdp_page_title = "${name}"
-end
-
-M.refactoring = function()
-
-    require('refactoring').setup({})
-
-    vim.api.nvim_create_user_command("ExtractFunction", function()
-        require("refactoring").refactor("Extract Function")
-    end, {
-        force = true,
-        range = true,
-    })
-    vim.api.nvim_create_user_command("ExtractVariable", function()
-        require("refactoring").refactor("Extract Variable")
-    end, {
-        force = true,
-        range = true,
-    })
-    vim.api.nvim_create_user_command("ExtractFunctionToFile", function()
-        require("refactoring").refactor("Extract Function To File")
-    end, {
-        force = true,
-        range = true,
     })
 end
 
