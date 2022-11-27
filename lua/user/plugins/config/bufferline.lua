86e95876a292feb0e867e58340b3fab8bfa0a265
Zahid Islam <dev@zahidislm.com>
2022-11-26 22:36:33 -0500

    [NVIM] Added bufferline config.

--- lua/user/plugins/config/bufferline.lua
+++ lua/user/plugins/config/bufferline.lua
@@ -1,1 +1,21 @@
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+            },
+        },
+        separator_style = "slant",
+        modified_icon = "",
+		color_icons = true,
+		show_buffer_icons = true,
+        enforce_regular_tabs = true,
+    },
+}
+
+-- Mappings
+require("user.plugins.mapping.bufferline")
