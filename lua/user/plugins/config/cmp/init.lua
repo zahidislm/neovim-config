dc0a7e47eab21047effae4313d84c45d7d149373
Zahid Islam <dev@zahidislm.com>
2022-11-26 22:31:12 -0500

    [NVIM] Completion provider: Snippy -> LuaSnip
    - refactored cmp to fix autocompletion errors, mappings, & style.

--- lua/user/plugins/config/cmp/init.lua
+++ lua/user/plugins/config/cmp/init.lua
@@ -1,110 +1,85 @@
-local feedkey = function(key, mode)
-    mode = mode or "n"
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            require('snippy').expand_snippet(args.body)
-        end,
-    },
-    window = {
-        documentation = {
-            winhighlight = "NormalFloat:CmpDocumentation,FloatBorder:CmpDocumentationBorder",
-        },
-    },
+cmp.setup {
     experimental = {
         ghost_text = true,
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "snippy" },
-        {
-            name = "buffer",
-            option = {
-                get_bufnrs = function()
-                    local bufs = {}
-                    for _, win in ipairs(vim.api.nvim_list_wins()) do
-                        bufs[vim.api.nvim_win_get_buf(win)] = true
-                    end
-                    return vim.tbl_keys(bufs)
-                end,
-            },
-        },
-        { name = "path"}
-    })
-})
-
-local cmdline_formatting = {
-    format = function(_, vim_item)
-        vim_item.kind = ""
-        vim_item.menu = ""
-        return vim_item
-    end,
-}
-
-local cmdline_mapping = cmp.mapping.preset.insert({
-    ["<C-j>"] = cmp.mapping({
-        c = function()
-            if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-            else
-                feedkey("<Down>")
-            end
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
         end,
-    }),
-    ["<C-k>"] = cmp.mapping({
-        c = function()
+    },
+    mapping = cmp.mapping.preset.insert {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        -- ["<ESC>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-            else
-                feedkey("<Up>")
-            end
-        end,
-    }),
-    ["<Tab>"] = cmp.mapping({
-        c = function(fallback)
-            if cmp.visible() then
-                cmp.confirm({ select = true })
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(replace_termcodes("<Plug>luasnip-expand-or-jump"), "")
             else
                 fallback()
             end
-        end,
-    }),
-})
-
-cmp.setup.cmdline("/", {
-    formatting = cmdline_formatting,
-    mapping = cmdline_mapping,
-    sources = cmp.config.sources({
-        { name = "buffer" },
-    }),
-})
-
-cmp.setup.cmdline(":", {
-    formatting = cmdline_formatting,
-    mapping = cmdline_mapping,
-    sources = cmp.config.sources({
-        { name = "cmdline" },
-        { name = "buffer" },
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = {
+        { name = "nvim_lua" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "path" },
-    }),
+        { name = "buffer" },
+    },
+}
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
 })
 
 -- Add nvim-autopairs support
 cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
 )
