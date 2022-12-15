7aa3bd8fd8bb86f363d4054ed851c1069bab5293
Zahid Islam <dev@zahidislm.com>
2022-12-15 13:31:16 -0500

    [NVIM] removed unused config.

--- lua/user/plugins/configs/lsp/signature.lua
+++
@@ -1,9 +0,0 @@
-require("lsp_signature").setup({
-    bind = true,
-    floating_window = true,
-    hi_parameter = "ThemerHeadingH1",
-    extra_trigger_chars = { "(", "," },
-    handler_opts = {
-        border = "single",
-    },
-})
