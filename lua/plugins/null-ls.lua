9a2ca6ac833fee0a1c62af44d2876260e98ea499
Zahid Islam <dev@zahidislm.com>
2023-01-26 21:12:49 -0500

    refactor(nvim.null-ls): tool setup handler
    - organized dependencies
    - setup tool init via auto handler
    - change debounce time to 250 ms
    - use lazy.nvim opts to organize configs
    - only load null-ls on certain filetypes (LSP supported for now)

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,34 +1,30 @@
-local M = {
-	"jay-babu/mason-null-ls.nvim",
-	event = "BufReadPost",
-	dependencies = {
-		"williamboman/mason.nvim",
-		{
-			"jose-elias-alvarez/null-ls.nvim",
-			config = function()
-				require("null-ls").setup({
-					debounce = 150,
-					save_after_format = false,
-				})
-			end,
+return {
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = {
+			debounce = 250,
+			save_after_format = false,
 		},
 	},
+
+	{
+		"jay-babu/mason-null-ls.nvim",
+		ft = LSP_FILETYPES,
+		dependencies = { "lsp", "mason" },
+		opts = {
+			ensure_installed = FORMATTERS,
+			automatic_installation = false,
+			automatic_setup = true,
+		},
+
+		config = function(_, opts)
+			require("mason-null-ls").setup(opts)
+
+			require("mason-null-ls").setup_handlers({
+				function(source_name, methods)
+					require("mason-null-ls.automatic_setup")(source_name, methods)
+				end,
+			})
+		end,
+	},
 }
-
-function M.config()
-	require("mason-null-ls").setup({
-		ensure_installed = FORMATTERS,
-		automatic_installation = false,
-		automatic_setup = true,
-	})
-
-	require("mason-null-ls").setup_handlers()
-end
-
-function M.has_formatter(ft)
-	local sources = require("null-ls.sources")
-	local available = sources.get_available(ft, "NULL_LS_FORMATTING")
-	return #available > 0
-end
-
-return M
