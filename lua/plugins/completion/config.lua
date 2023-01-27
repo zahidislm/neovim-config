1437b0c13cc1df2170b1a9f028d98091d9c36df2
Zahid Islam <dev@zahidislm.com>
2023-01-26 19:57:10 -0500

    refactor(nvim.completion.config): cleanup
    - removed unused sources
    - re-wrote keymap handling for <TAB>
    - indentation formatting

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -1,114 +1,91 @@
 local cmp = require("cmp")
-local luasnip = require("luasnip")
 
-local function replace_termcodes(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local M = {}
-
-function M.setup()
-	cmp.setup({
-		experimental = {
-			ghost_text = true,
+return {
+	experimental = {
+		ghost_text = {
+			hl_group = "LspCodeLens",
 		},
+	},
 
-		window = {
-			completion = {
-				winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-				col_offset = -3,
-				side_padding = 0,
-			},
+	window = {
+		completion = {
+			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+			col_offset = -3,
+			side_padding = 0,
 		},
+	},
 
-		snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 
-		formatting = {
-			fields = { "kind", "abbr", "menu" },
-			format = function(entry, vim_item)
-				local kind =
-					require("lspkind").cmp_format({ mode = "symbol_text", preset = "codicons", maxwidth = 50 })(
-						entry,
-						vim_item
-					)
-				local strings = vim.split(kind.kind, "%s", { trimempty = true })
-				kind.kind = " " .. strings[1] .. " "
-				kind.menu = "    " .. strings[2] .. ""
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", preset = "codicons", maxwidth = 50 })(
+				entry,
+				vim_item
+			)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. strings[1] .. " "
+			kind.menu = "    " .. strings[2] .. ""
 
-				return vim_item
-			end,
-		},
+			return vim_item
+		end,
+	},
 
-		mapping = cmp.mapping.preset.insert({
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-d>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			["<CR>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true,
-			}),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expand_or_jumpable() then
-					vim.fn.feedkeys(replace_termcodes("<Plug>luasnip-expand-or-jump"), "")
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
 		}),
 
-		sources = {
-			{ name = "nvim_lua" },
-			{ name = "nvim_lsp" },
-			{ name = "nvim_lsp_signature_help" },
-			{ name = "luasnip" },
-			{ name = "path" },
-			{ name = "buffer" },
-		},
-	})
+		["<Tab>"] = cmp.mapping(function(fallback)
+			local luasnip = require("luasnip")
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
-	-- Setup "/" commands
-	cmp.setup.cmdline("/", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = {
-			{ name = "buffer" },
-		},
-	})
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 
-	-- Setup ":" commands
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-			{ name = "cmdline", option = { ignore_cmds = { "Man", "!" } } },
-		}),
-	})
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			local luasnip = require("luasnip")
 
-	-- Setup vim.input commands
-	cmp.setup.cmdline("@", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-			{ name = "cmdline" },
-		}),
-	})
-end
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
 
-return M
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	},
+}
