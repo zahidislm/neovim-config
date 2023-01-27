1d375ce30248f1683a2ec964d8f27cf809711977
Zahid Islam <dev@zahidislm.com>
2023-01-26 19:54:29 -0500

    refactor(nvim.completion): use new lazy.nvim opts
    - organized dependencies to load lazier.
    - removed unused cmp sources.

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,38 +1,36 @@
-local M = {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
-				},
-			},
-
-			config = function()
-				require("luasnip").setup({
-					history = true,
-					enable_autosnippets = true,
-				})
-			end,
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
 		},
 
-		"hrsh7th/cmp-nvim-lsp",
-		"onsails/lspkind.nvim",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-path",
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+
+		config = function(_, opts)
+			require("luasnip").setup(opts)
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		version = false,
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+
+		opts = function()
+			return require("plugins.completion.config")
+		end,
 	},
 }
-
-function M.config()
-	require("plugins.completion.config").setup()
-end
-
-return M
