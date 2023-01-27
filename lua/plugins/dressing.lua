ffa340a961c8961f4580ebdf3f2e2e63112188b3
Zahid Islam <dev@zahidislm.com>
2023-01-26 20:43:26 -0500

    refactor(nvim.dressing): even more lazier.
    - load dressings even lazier only when vim.ui is called
    - custom settings for codeactions

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,10 +1,11 @@
-local M = { "stevearc/dressing.nvim", event = "VeryLazy" }
+return {
+	"stevearc/dressing.nvim",
 
-function M.config()
-	require("dressing").setup({
+	opts = {
 		select = {
 			get_config = function(opts)
 				opts = opts or {}
+
 				local cfg = {
 					telescope = {
 						layout_config = {
@@ -14,18 +15,34 @@ function M.config()
 					},
 				}
 
+				if opts.kind == "codeaction" then
+					cfg.telescope =
+						require("telescope.themes").get_cursor({ layout_config = { height = 0.15, width = 0.45 } })
+				end
+
 				return cfg
 			end,
 		},
 
 		input = {
 			override = function(conf)
-				conf.col = -1
-				conf.row = 0
+				conf.col = 0
+				conf.row = -2
 				return conf
 			end,
 		},
-	})
-end
+	},
 
-return M
+	init = function()
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.ui.select = function(...)
+			require("lazy").load({ plugins = { "dressing.nvim" } })
+			return vim.ui.select(...)
+		end
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.ui.input = function(...)
+			require("lazy").load({ plugins = { "dressing.nvim" } })
+			return vim.ui.input(...)
+		end
+	end,
+}
