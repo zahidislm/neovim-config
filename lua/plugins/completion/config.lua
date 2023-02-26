3d52ebaffc346d3639a6468a5bc303d87801e29b
Zahid Islam <dev@zahidislm.com>
2023-02-26 17:42:42 -0500

    fix(completion): revert to default icons

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -24,10 +24,7 @@ return {
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
-			local kind = require("lspkind").cmp_format({ mode = "symbol_text", preset = "codicons", maxwidth = 50 })(
-				entry,
-				vim_item
-			)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
 			local strings = vim.split(kind.kind, "%s", { trimempty = true })
 			kind.kind = " " .. strings[1] .. " "
 			kind.menu = "    " .. strings[2] .. ""
