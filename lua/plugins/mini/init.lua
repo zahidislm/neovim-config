03b66527464f4e5aada661c6b8afb04189f4d633
Zahid Islam <dev@zahidislm.com>
2023-05-24 13:08:23 -0400

    fix(mini): removed custom logic for lazy.nvim
    - removed version logic since default behavior is already enforced

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -18,28 +18,17 @@ local function init_mini(conf)
 	for pos, mod in ipairs(modules) do
 		local mod_url = "echasnovski/mini." .. mod
 		local configuration = conf[mod] or {}
-		local mod_version = "*"
-
-		if configuration["version"] ~= nil then
-			mod_version = configuration["version"]
-		end
-
 		local mod_keys = configuration["keys"] or nil
 		local mod_event = configuration["event"] or "BufReadPost"
 		local mod_dependencies = configuration["dependencies"] or nil
 		local mod_opts = configuration["opts"] or {}
-		local mod_config = function(_, opts)
-			require("mini." .. mod).setup(opts)
-		end
 
 		local mod_spec = {
 			mod_url,
-			version = mod_version,
 			event = mod_event,
 			keys = mod_keys,
 			dependencies = mod_dependencies,
 			opts = mod_opts,
-			config = mod_config,
 		}
 
 		table.insert(spec, pos, mod_spec)
