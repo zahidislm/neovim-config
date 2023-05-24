b373c8abdfc8cee8b84371896e5e87736decf9b0
Zahid Islam <dev@zahidislm.com>
2023-05-24 13:03:38 -0400

    fix(null-ls): update handler logic.
    - mason-null-ls no longer requires custom handler logic.
    - init w/ event instead of filetype.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -9,22 +9,12 @@ return {
 
 	{
 		"jay-babu/mason-null-ls.nvim",
-		ft = LSP_FILETYPES,
+		event = "BufReadPre",
 		dependencies = { "lsp", "mason" },
 		opts = {
 			ensure_installed = FORMATTERS,
 			automatic_installation = false,
-			automatic_setup = true,
+			handlers = {},
 		},
-
-		config = function(_, opts)
-			require("mason-null-ls").setup(opts)
-
-			require("mason-null-ls").setup_handlers({
-				function(source_name, methods)
-					require("mason-null-ls.automatic_setup")(source_name, methods)
-				end,
-			})
-		end,
 	},
 }
