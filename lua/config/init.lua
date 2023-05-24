432e60ea0a904f079e64e3a049916d296c537873
Zahid Islam <dev@zahidislm.com>
2023-05-24 12:34:00 -0400

    refactor(config): renamed commands -> autocmds

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,7 +4,7 @@ require("config.options")
 vim.api.nvim_create_autocmd("User", {
 	pattern = "VeryLazy",
 	callback = function()
-		require("config.commands")
+		require("config.autocmds")
 		require("config.keymaps")
 	end,
 })
