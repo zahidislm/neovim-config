131545cc7aee16f4aec7315b5293ab953deee6e4
Zahid Islam <dev@zahidislm.com>
2023-06-22 00:53:19 -0400

    feat(keymaps): keymaps, new keymaps!
    - search n' replace w/ c operator
    - quickfix QOL

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,15 +12,19 @@ local e_opts = { expr = true }
 -- default replcements
 keymap(norm_vis, ";", ":", { silent = false })
 keymap(norm, "x", '"_x') -- delete without putting into clipboard
+keymap(norm_vis, "c", '"_c') -- change without putting into clipboard
+keymap(norm_vis, "C", '"_C') -- change til EOL without putting into clipboard
 keymap(vis, "d", '"_d') -- delete without putting into clipboard
 keymap(vis, "D", "d") -- original d keymap
 
--- insert movement
-keymap(ins, "<M-a>", "<Esc>_i", { desc = "Move to the start of line" })
-keymap(ins, "<M-e>", "<Esc>g_a", { desc = "Move to the end of line" })
+-- line pos movement
+keymap(ins, "<M-H>", "<Esc>_i", { desc = "Move to the start of line" })
+keymap(ins, "<M-L>", "<Esc>g_a", { desc = "Move to the end of line" })
+keymap(norm, "H", "^") -- move to start of line
+keymap(norm, "L", "$") -- move to end of line
 
 -- clear search with esc
-keymap(ins_norm, "<esc>", "<Cmd>noh<CR><esc>")
+keymap(ins_norm, "<esc>", "<Cmd>noh<CR><Esc>")
 keymap(norm, "gw", "*N")
 keymap("x", "gw", "*N")
 
@@ -46,6 +50,17 @@ keymap(norm, "<A-]>", ">>", { desc = "Indent forwards" })
 keymap(vis, "<A-[>", "<gv", { desc = "Indent backwards" })
 keymap(vis, "<A-]>", ">gv", { desc = "Indent forwards" })
 
+-- Quickfix
+keymap(norm, "qf", "<Cmd>copen<CR>", { desc = "open quickfix window" })
+keymap(norm, "qfc", "<Cmd>call setqflist([], 'r')<CR>", { desc = "Clear current quickfix list" })
+keymap(norm, "<Left>", "<Cmd>cnext<CR>", { desc = "Swap to next quickfix list" })
+keymap(norm, "<Right>", "<Cmd>cprev<CR>", { desc = "Swap to previous quickfix list" })
+
+-- Utils
+keymap(ins, "<M-BS>", "<Esc>cvb", { desc = "delete previous word" })
+keymap(norm, "<C-q>", "<Cmd>q!<CR>", { desc = "Force quit" })
+keymap(ins, "<M-d>", "<Esc>yypgi", { desc = "Duplicates line" })
+
 -- LEADER KEYMAPS --
 -- window management
 keymap(norm, "<Leader>ww", "<C-W>p", { desc = "other-window" })
@@ -59,6 +74,14 @@ keymap(norm, "<Leader>w=", "<C-W>=", { desc = "balance-window" })
 keymap(norm, "<Leader>ws", "<C-W>s", { desc = "split-window-below" })
 keymap(norm, "<Leader>wv", "<C-W>v", { desc = "split-window-right" })
 
+-- Search & Replace
+keymap(norm, "c*", "*``cgn", { desc = "Renames cursorword downward" })
+keymap(vis, "c*", '"fy/\\V<C-R>f<CR>``cgn', { desc = "Renames cursorword downward" })
+keymap(norm, "c#", "#``cgN", { desc = "Renames cursorword upwards" })
+keymap(vis, "c#", '"fy/\\V<C-R>f<CR>``cgN', { desc = "Renames cursorword upwards" })
+keymap(norm, "<Leader>r", [["zyiw:%s/<C-r>z//g<Left><Left>]], { desc = "Replaces all instance of cursorword" })
+keymap(vis, "<Leader>r", [["zy:%s/<C-r>z//g<Left><Left>]], { desc = "Replaces all instance of cursorword" })
+
 -- buffers
 keymap(norm, "<Leader>bc", "<Cmd>%bd|e#|bd#<CR>", { desc = "delete all other buffers" })
 
@@ -71,4 +94,4 @@ keymap(norm, "<Leader>to", function()
 	util.float_term()
 end, { desc = "open floating term (cwd)" })
 
-keymap("t", "<esc><esc>", "<C-\\><c-n>", { desc = "Enter Normal Mode" })
+keymap("t", "<Esc><Esc>", "<C-\\><c-n>", { desc = "Enter Normal Mode" })
