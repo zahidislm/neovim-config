d046e81c7dfe69e5b0c4267babe80633f0829f68
Zahid Islam <dev@zahidislm.com>
2023-06-22 01:07:37 -0400

    fix(yanky): increased highlight animation duration

--- lua/plugins/yanky/init.lua
+++ lua/plugins/yanky/init.lua
@@ -1,12 +1,9 @@
 return {
 	"gbprod/yanky.nvim",
+	opts = {
+		highlight = { timer = 200 },
+	},
 	keys = function()
 		return require("plugins.yanky.keys")
 	end,
-
-	opts = {
-		highlight = {
-			timer = 180,
-		},
-	},
 }
