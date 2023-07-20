308a6d930aed46a69e266bf5fec81fc8fa9358ca
Zahid Islam <dev@zahidislm.com>
2023-07-19 23:21:35 -0400

    refactor(keymaps): changes & organization
    - Added quickfix keymaps
    - added search default replacement
    - removed : ; swap for default behavior
    - fixed cmd opts for floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,31 +9,36 @@ local ins_norm = { "i", "n" }
 
 local e_opts = { expr = true }
 
--- default replcements
-keymap(norm_vis, ";", ":", { silent = false })
-keymap(norm, "x", '"_x') -- delete without putting into clipboard
-keymap(norm_vis, "c", '"_c') -- change without putting into clipboard
-keymap(norm_vis, "C", '"_C') -- change til EOL without putting into clipboard
-keymap(vis, "d", '"_d') -- delete without putting into clipboard
-keymap(vis, "D", "d") -- original d keymap
+-- default replacements
+keymap(norm, "x", [["_x]]) -- delete without putting into clipboard
+keymap(norm_vis, "c", [["_c]]) -- change without putting into clipboard
+keymap(norm_vis, "C", [["_C]]) -- change til EOL without putting into clipboard
+keymap(norm_vis, "d", [["_d]]) -- delete without putting into clipboard
+keymap(norm_vis, "D", "d") -- original d keymap
+keymap(norm, "<C-d>", "<C-d>zz") -- center on page down
+keymap(norm, "<C-u>", "<C-u>zz") -- center on page up
+keymap(norm, "/", "zn/", { silent = false }) -- search & pause folds
+
+-- visual mode parity
+keymap(vis, "&", ":&&<cr>") -- visual execute last substitution
+keymap(vis, ".", ":normal! .<cr>") -- visual execute .
+keymap(vis, "@", ":normal! @") -- visual execute macro
 
 -- line pos movement
 keymap(ins, "<M-H>", "<Esc>_i", { desc = "Move to the start of line" })
 keymap(ins, "<M-L>", "<Esc>g_a", { desc = "Move to the end of line" })
-keymap(norm, "H", "^") -- move to start of line
-keymap(norm, "L", "$") -- move to end of line
+keymap(norm, "gh", "^", { desc = "Move to the start of line" })
+keymap(norm, "gl", "$", { desc = "Move to the end of line" })
 
 -- clear search with esc
 keymap(ins_norm, "<esc>", "<Cmd>noh<CR><Esc>")
-keymap(norm, "gw", "*N")
-keymap("x", "gw", "*N")
 
 -- n to always search forward and N backward
 keymap(norm, "n", "'Nn'[v:searchforward]", e_opts)
-keymap("x", "n", "'Nn'[v:searchforward]", e_opts)
+keymap(vis, "n", "'Nn'[v:searchforward]", e_opts)
 keymap("o", "n", "'Nn'[v:searchforward]", e_opts)
 keymap(norm, "N", "'nN'[v:searchforward]", e_opts)
-keymap("x", "N", "'nN'[v:searchforward]", e_opts)
+keymap(vis, "N", "'nN'[v:searchforward]", e_opts)
 keymap("o", "N", "'nN'[v:searchforward]", e_opts)
 
 -- Add undo break-points
@@ -45,20 +50,13 @@ keymap(ins, ";", ";<C-g>u")
 keymap(norm, "<Leader><Space>", "za", { desc = "Toggle code folding" })
 
 -- Indenting
-keymap(norm, "<A-[>", "<<", { desc = "Indent backwards" })
-keymap(norm, "<A-]>", ">>", { desc = "Indent forwards" })
-keymap(vis, "<A-[>", "<gv", { desc = "Indent backwards" })
-keymap(vis, "<A-]>", ">gv", { desc = "Indent forwards" })
-
--- Quickfix
-keymap(norm, "qf", "<Cmd>copen<CR>", { desc = "open quickfix window" })
-keymap(norm, "qfc", "<Cmd>call setqflist([], 'r')<CR>", { desc = "Clear current quickfix list" })
-keymap(norm, "<Left>", "<Cmd>cnext<CR>", { desc = "Swap to next quickfix list" })
-keymap(norm, "<Right>", "<Cmd>cprev<CR>", { desc = "Swap to previous quickfix list" })
+keymap(vis, "<<", "<gv", { desc = "Indent backwards" })
+keymap(vis, ">>", ">gv", { desc = "Indent forwards" })
 
 -- Utils
-keymap(ins, "<M-BS>", "<Esc>cvb", { desc = "delete previous word" })
+keymap(ins, "<M-BS>", [[<Esc>"_cvb]], { desc = "delete previous word" })
 keymap(norm, "<C-q>", "<Cmd>q!<CR>", { desc = "Force quit" })
+keymap(norm, "<C-Q>", "<Cmd>qa!<CR>", { desc = "Force quit all" })
 keymap(ins, "<M-d>", "<Esc>yypgi", { desc = "Duplicates line" })
 
 -- LEADER KEYMAPS --
@@ -74,6 +72,10 @@ keymap(norm, "<Leader>w=", "<C-W>=", { desc = "balance-window" })
 keymap(norm, "<Leader>ws", "<C-W>s", { desc = "split-window-below" })
 keymap(norm, "<Leader>wv", "<C-W>v", { desc = "split-window-right" })
 
+-- Quickfix
+keymap(norm, "<Leader>qo", "<Cmd>copen<CR>", { desc = "Open quickfix window" })
+keymap(norm, "<Leader>qc", "<Cmd>call setqflist([], 'r')<CR>", { desc = "Clear current quickfix list" })
+
 -- Search & Replace
 keymap(norm, "c*", "*``cgn", { desc = "Renames cursorword downward" })
 keymap(vis, "c*", '"fy/\\V<C-R>f<CR>``cgn', { desc = "Renames cursorword downward" })
@@ -87,11 +89,9 @@ keymap(norm, "<Leader>bc", "<Cmd>%bd|e#|bd#<CR>", { desc = "delete all other buf
 
 -- floating terminal
 keymap(norm, "<Leader>tO", function()
-	util.float_term(nil, { cwd = util.get_root() })
+	util.float_term()
 end, { desc = "open floating term (root)" })
 
 keymap(norm, "<Leader>to", function()
-	util.float_term()
+	util.float_term(nil, { cwd = vim.fn.expand("%:p:h") })
 end, { desc = "open floating term (cwd)" })
-
-keymap("t", "<Esc><Esc>", "<C-\\><c-n>", { desc = "Enter Normal Mode" })
