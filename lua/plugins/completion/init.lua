6b51c178665f9f08ea77ccff5e7ab31cacfbaa18
Zahid Islam <dev@zahidislm.com>
2023-07-20 01:19:20 -0400

    feat(auto-pairs): added auto-pair plugin
    - allow custom matching for < > if right after alpha-numeric

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -20,4 +20,32 @@ return {
 		end,
 		version = false,
 	},
+
+	{
+		"windwp/nvim-autopairs",
+		opts = {
+			fast_wrap = {},
+			disable_filetype = { "fzf", "vim" },
+		},
+		config = function(_, opts)
+			local autopairs = require("nvim-autopairs")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+
+			autopairs.setup(opts)
+			autopairs.add_rules({
+				rule("<", ">"):with_pair(cond.before_regex("%a+")):with_move(function(move_opts)
+					return move_opts.char == ">"
+				end),
+			})
+
+			-- setup cmp for autopairs
+			local has_cmp, cmp = pcall(require, "cmp")
+			if has_cmp then
+				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			end
+		end,
+		event = "InsertEnter",
+	},
 }
