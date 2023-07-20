52d4fb482f5ab8cbc8c75923e3ce1ddd9868400e
Zahid Islam <dev@zahidislm.com>
2023-07-20 01:13:48 -0400

    refactor(cmp): LuaSnip -> nvim-snippy
    - use nvim-snippy as snippet handler
    - use async-path source
    - add borders
    - comparator sorting based on weighted priority
    - don't preselect
    - organized code
    - no json based snippets
    - use icons from ui module

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -1,30 +1,30 @@
+local icons = vim.g.ui_icons
 local cmp = require("cmp")
+local snippy = require("snippy")
+
+local function highlight_border(hl_name)
+	hl_name = type(hl_name) == "string" and hl_name or "CmpBorder"
+	local border = icons.misc.border
+	local border_with_hls = {}
+
+	for char = 1, #border do
+		border_with_hls[char] = { border[char], hl_name }
+	end
+
+	return border_with_hls
+end
 
 return {
-	experimental = {
-		ghost_text = {
-			hl_group = "LspCodeLens",
-		},
-	},
-
-	window = {
-		completion = {
-			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-			col_offset = -3,
-			side_padding = 0,
-		},
-	},
-
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-
+	experimental = { ghost_text = { hl_group = "LspCodeLens" } },
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
-			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local kind = require("lspkind").cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50,
+				symbol_map = icons.symbol_map,
+				ellipsis_char = "...",
+			})(entry, vim_item)
 			local strings = vim.split(kind.kind, "%s", { trimempty = true })
 			kind.kind = " " .. strings[1] .. " "
 			kind.menu = "    " .. strings[2] .. ""
@@ -32,32 +32,30 @@ return {
 			return vim_item
 		end,
 	},
-
 	mapping = cmp.mapping.preset.insert({
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i" }),
+		["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i" }),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
+			select = false,
 		}),
 
 		["<Tab>"] = cmp.mapping(function(fallback)
-			local luasnip = require("luasnip")
 			local has_words_before = function()
 				unpack = unpack or table.unpack
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+				local match = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+				return col ~= 0 and match == nil
 			end
 
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
+			elseif snippy.can_expand_or_advance() then
+				snippy.expand_or_advance()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -66,23 +64,64 @@ return {
 		end, { "i", "s" }),
 
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			local luasnip = require("luasnip")
-
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+			elseif snippy.can_jump(-1) then
+				snippy.previous()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
 	}),
+	preselect = cmp.PreselectMode.None,
+	snippet = {
+		expand = function(args)
+			snippy.expand_snippet(args.body)
+		end,
+	},
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			cmp.config.compare.recently_used,
+			function(entry1, entry2)
+				local _, entry1_under = entry1.completion_item.label:find("^_+")
+				local _, entry2_under = entry2.completion_item.label:find("^_+")
+				entry1_under = entry1_under or 0
+				entry2_under = entry2_under or 0
+				if entry1_under > entry2_under then
+					return false
+				elseif entry1_under < entry2_under then
+					return true
+				end
+			end,
 
+			cmp.config.compare.kind,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lsp_signature_help" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "path" },
+		{ name = "nvim_lsp", priority = 1000 },
+		{ name = "nvim_lsp_signature_help", priority = 900 },
+		{ name = "snippy", priority = 750 },
+		{ name = "buffer", keyword_length = 5, priority = 500 },
+		{ name = "async_path", priority = 250 },
+	},
+	window = {
+		completion = {
+			winhighlight = "Normal:NormalFloat,Search:None",
+			border = highlight_border(),
+			col_offset = -4,
+			side_padding = 0,
+			scrollbar = false,
+		},
+		documentation = {
+			winhighlight = "Normal:NormalFloat,Search:None",
+			border = highlight_border(),
+			max_height = 15,
+			max_width = 60,
+		},
 	},
 }
--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,33 +1,23 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-		},
-		config = function(_, opts)
-			require("luasnip").setup(opts)
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
-
-	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			-- snippet engine
+			"dcampos/nvim-snippy",
+			"dcampos/cmp-snippy",
+
+			-- sources
 			"hrsh7th/cmp-nvim-lsp",
-			"saadparwaiz1/cmp_luasnip",
-			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
+			"FelipeLema/cmp-async-path",
+
+			-- misc
+			"onsails/lspkind.nvim",
 		},
 		opts = function()
 			return require("plugins.completion.config")
 		end,
 		version = false,
-		event = "insertenter",
 	},
 }
