63a069d5479a467ac7e455d514ad9ffcd9c79260
Zahid Islam <dev@zahidislm.com>
2026-06-24 20:39:37 -0400

    refactor(lsp): inline lsp keymaps to plugin/lsp.lua

--- plugin/05-lsp.lua
+++ plugin/05-lsp.lua
@@ -1,3 +1,51 @@
+-- Keymap helper
+local function keymap(mode, lhs, rhs, bufnr, desc)
+  vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+end
+
+local function global_provider(bufnr)
+  keymap(
+    "n", "gri", "<Cmd>Glance implementations<CR>", bufnr, "Show implementations of the cursorword"
+  )
+
+  keymap("n", "grr", "<Cmd>Glance references<CR>", bufnr, "Show references of the cursorword")
+
+  keymap(
+    "n", "grt", "<Cmd>Glance type_definitions<CR>", bufnr, "Show type definitions of the cursorword"
+  )
+
+  keymap("n", "grO", "<Cmd>Glance definitions<CR>", bufnr, "Show definition of the cursorword")
+end
+
+local function rename_provider(client, bufnr)
+  local caps = client ~= nil and client.server_capabilities or {}
+
+  if caps.renameProvider then
+    keymap("n", "grN", function ()
+      require("live-rename").rename({ text = "", insert = true })
+    end, bufnr, "change LSP symbol"
+    )
+
+    keymap("n", "grn", function ()
+      require("live-rename").rename()
+    end, bufnr, "edit LSP symbol"
+    )
+  end
+end
+
+local function format_provider(client, bufnr)
+  local caps = client ~= nil and client.server_capabilities or {}
+
+  if caps.documentFormattingProvider then
+    local modes = caps.documentRangeFormattingProvider and { "n", "v" } or { "n" }
+    keymap(modes, "grf", function ()
+      vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 3000 })
+      if vim.fn.exists(":GuessIndent") then vim.cmd("silent GuessIndent") end
+    end, bufnr, "format code"
+    )
+  end
+end
+
 -- Global LSP Config
 vim.lsp.config("*", { root_markers = { ".git" } })
 
@@ -17,11 +65,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local bufnr = args.buf
     local client_id = args.data.client_id
     local client = vim.lsp.get_client_by_id(client_id)
-    local lsp_keymap = require("config.keymaps.lsp")
 
     -- Keymaps
-    lsp_keymap.global_provider(bufnr)
-    lsp_keymap.rename_provider(client, bufnr)
-    lsp_keymap.format_provider(client, bufnr)
+    global_provider(bufnr)
+    rename_provider(client, bufnr)
+    format_provider(client, bufnr)
   end,
 })
