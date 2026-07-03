f1dc53880344bd0f744416442ac5b5d08a6037fd
Zahid Islam <dev@zahidislm.com>
2026-07-03 12:12:45 -0400

    fix(plugin.diagnostics-hover): lazy load plugin on autocmd.

--- plugin/04-diagnostics-hover.lua
+++ plugin/04-diagnostics-hover.lua
@@ -1,22 +1,20 @@
-local diaghover = require("ui.diag-hover")
-diaghover.setup({})
-
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
-  callback = diaghover.__generate_highlights,
+  callback =  require("ui.diag-hover").__generate_highlights,
 })
 
 if vim.v.vim_did_enter == 1 then
-  diaghover.__generate_highlights()
+  require("ui.diag-hover").__generate_highlights()
 else
   vim.api.nvim_create_autocmd("VimEnter", {
     once = true,
-    callback = diaghover.__generate_highlights,
+    callback =  require("ui.diag-hover").__generate_highlights,
   })
 end
 
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
   callback = function ()
+    local diaghover = require("ui.diag-hover")
     if diaghover.window and vim.api.nvim_get_current_win() ~= diaghover.window then
       diaghover.close()
     end
