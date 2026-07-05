1bfd5507a4fc1a02669a77b39f9882f1eb8be642
Zahid Islam <dev@zahidislm.com>
2026-07-05 15:09:06 -0400

    fix(ui.*-hover): Fix highlights not applying on 'ColorScheme'

--- plugin/04-diagnostics-hover.lua
+++ plugin/04-diagnostics-hover.lua
@@ -1,14 +1,18 @@
+local function schedule_highlight_gen()
+  vim.defer_fn(require("ui.diag-hover").__generate_highlights, 20)
+end
+
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
-  callback =  require("ui.diag-hover").__generate_highlights,
+  callback = schedule_highlight_gen,
 })
 
 if vim.v.vim_did_enter == 1 then
-  require("ui.diag-hover").__generate_highlights()
+  schedule_highlight_gen()
 else
   vim.api.nvim_create_autocmd("VimEnter", {
     once = true,
-    callback =  require("ui.diag-hover").__generate_highlights,
+    callback = schedule_highlight_gen,
   })
 end
 
